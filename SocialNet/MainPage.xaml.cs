146236e2f76bd43892acb419efc7e177d38c9120
Mikhail <miha.vanya@gmail.com>
2018-01-10 00:53:10 +0500

    broke something

--- SocialNet/MainPage.xaml.cs
+++ SocialNet/MainPage.xaml.cs
@@ -1,30 +1,184 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Pickers.Provider;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SocialNet
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public ObservableCollection<User> AllUsers = new ObservableCollection<User>();
+
+        public User ActiveUser;
+        public ObservableCollection<User> ActiveUserSubscriptions;
+
+        public ObservableCollection<NewsItem> Posts;
+        private bool displayingUserPosts = false;
+
+        public NewsItem NewNewsItem = new NewsItem();
+
+        private void UpdateCollections()
+        {
+            ActiveUserSubscriptions = new ObservableCollection<User>(ActiveUser.Subscriptions);
+            FriendsList.ItemsSource = ActiveUserSubscriptions;
+            FriendsList.UpdateLayout();
+            
+            Posts = new ObservableCollection<NewsItem>(displayingUserPosts? ActiveUser.News.UserPosts: ActiveUser.News.Feed);
+
+            NewsFeed.ItemsSource = Posts;
+            NewsFeed.UpdateLayout();
+        }
+        private void NewsFeedLabel_OnTapped(object sender, PointerRoutedEventArgs e) { }
+        
+        private void AddFriendButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: new friends picker
+            
+            User r;
+
+            do
+                r = AllUsers[Generate.Int(AllUsers.Count)];
+            while (ActiveUserSubscriptions.Contains(r) || ActiveUser == r);
+
+            ActiveUser.Subscribe(r);
+            UpdateCollections();
+        }
+        private async void FriendItemName_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            MessageDialog dialog = new MessageDialog($"Do you really wish to remove {ActiveUserSubscriptions[FriendsList.SelectedIndex]} from your friends?", "Are you sure");
+            dialog.Commands.Add(new UICommand("Yes"));
+            dialog.Commands.Add(new UICommand("No"));
+
+            if((await dialog.ShowAsync()).Label == "No")
+                return;
+            ActiveUser.UnSubscribe(ActiveUserSubscriptions[FriendsList.SelectedIndex]);
+            UpdateCollections();
+        }
+
+        private async void NewUser_Click(object sender, RoutedEventArgs e)
+        {
+            AllUsers.Add(new User(await Person.MakeNew()));
+            
+            ChooseUser.SelectedIndex = AllUsers.Count;
+        }
+        private void ChooseUser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ActiveUser = (User)e.AddedItems.Last();
+            UserName.Content = ActiveUser.ToString();
+
+            UpdateCollections();
+        }
+        
+        private void PostNewNewsItem_Click(object sender, RoutedEventArgs e)
+        {
+            NewNewsItem.publisher = ActiveUser;
+            NewNewsItem.Content = NewNewsText.Text;
+            NewNewsItem.publishTime = DateTime.UtcNow;
+            ActiveUser.AddPost(NewNewsItem);
+            UpdateCollections();
+
+            NewNewsText.Text = string.Empty;
+            AddImageToNewNewsItem.Content = "+";
+            
+            NewNewsItem = new NewsItem();
+        }
+        private async void AddImageToNewNewsItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewNewsItem.Image != null)
+            {
+                NewNewsItem.Image = null;
+                AddImageToNewNewsItem.Content = "+";
+                return;
+            }
+            var p = new FileOpenPicker();
+
+            p.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            p.ViewMode = PickerViewMode.Thumbnail;
+            p.FileTypeFilter.Add(".jpg");
+            p.FileTypeFilter.Add(".jpeg");
+            p.FileTypeFilter.Add(".png");
+            p.FileTypeFilter.Add(".tiff");
+            p.FileTypeFilter.Add(".gif");
+
+            var p1 = await p.PickSingleFileAsync();
+            if (p1 == null)
+                return;
+            NewNewsItem.Image = new BitmapImage(new Uri(p1.Path, UriKind.Absolute));
+            AddImageToNewNewsItem.Content = "-";
+        }
+        private async void NewsPost_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (Posts[NewsFeed.SelectedIndex].publisher == ActiveUser)
+            {
+                MessageDialog dialog = new MessageDialog($"Do you really wish to remove this post?", "Are you sure");
+                dialog.Commands.Add(new UICommand("Yes"));
+                dialog.Commands.Add(new UICommand("No"));
+
+                if ((await dialog.ShowAsync()).Label == "No")
+                    return;
+
+                ActiveUser.RemovePost(Posts[NewsFeed.SelectedIndex]);
+                UpdateCollections();
+            }
+        }
+
+        private void UserName_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            displayingUserPosts = !displayingUserPosts;
+            if (displayingUserPosts)
+                NewsFeedLabel.Text = "Your Posts";
+            else
+                NewsFeedLabel.Text = "News Feed";
+            UpdateCollections();
+        }
+
+        private async void MainGrid_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            int UsersAdded = 6;
+
+            for (int i = 0; i < UsersAdded; i++)
+                AllUsers.Add(new User(await Person.MakeNew()));
+
+            AllUsers[1].Subscribe(AllUsers[5]);
+            AllUsers[0].Subscribe(AllUsers[1]);
+            AllUsers[2].Subscribe(AllUsers[4]);
+            AllUsers[4].Subscribe(AllUsers[5]);
+            AllUsers[1].Subscribe(AllUsers[2]);
+            AllUsers[0].Subscribe(AllUsers[3]);
+            AllUsers[0].Subscribe(AllUsers[5]);
+            AllUsers[2].Subscribe(AllUsers[3]);
+
+
+
+            
+            ChooseUser.SelectedIndex = 0;
+
+
+            //TODO: add a testing thingy
+
         }
     }
 }
---
+++ SocialNet/Person.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+
+namespace SocialNet
+{
+    public enum eGender
+    {
+        Male,
+        Female
+    }
+
+    public enum eRelationshipStatus
+    {
+        Single, 
+        Taken, 
+        Married, 
+        NotMarried
+    }
+
+    public class Person
+    {
+        public string First { get; protected set; }
+        public string Last { get; protected set; }
+        public eGender PersonGender { get; protected set; }
+        public DateTime DateOfBirth { get; protected set; }
+
+        public eRelationshipStatus RelationshipStatus { get; protected set; }
+        public string School { get; protected set; }
+        public string University { get; protected set; }
+
+        public Person() { }
+        public Person(eGender PersonGender, string First, string Last, DateTime DateOfBirth,
+            eRelationshipStatus RelationshipStatus, string School = null, string University = null)
+        {
+            this.PersonGender = PersonGender;
+            this.First = First;
+            this.Last = Last;
+            this.DateOfBirth = DateOfBirth;
+            this.RelationshipStatus = RelationshipStatus;
+            this.School = School;
+            this.University = University;
+        }
+
+        static List<string> MaleFirstNames = new List<string>(0);
+        static List<string> MaleLastNames = new List<string>(0);
+        static List<string> FemaleFirstNames = new List<string>(0);
+        static List<string> FemaleLastNames = new List<string>(0);
+
+        public static async Task<IList<string>> ReadFile(string path) => await FileIO.ReadLinesAsync(await StorageFile.GetFileFromApplicationUriAsync(new Uri($@"ms-appx:///{path}")));
+
+        public async Task<Person> FillBlanks(eGender PersonGender)
+        {
+            switch (PersonGender)
+            {
+                case eGender.Male:
+                {
+                    if (MaleFirstNames.Count == 0)
+                        MaleFirstNames = (await ReadFile(@"=malefirstnames.txt")).ToList();
+
+                    if (MaleLastNames.Count == 0)
+                        MaleLastNames = (await ReadFile(@"=malelastnames.txt")).ToList();
+
+                    First = MaleFirstNames[Generate.Int(MaleFirstNames.Count)];
+                    Last = MaleLastNames[Generate.Int(MaleLastNames.Count)];
+                    break;
+                }
+
+                case eGender.Female:
+                {
+                    if (FemaleFirstNames.Count == 0)
+                        FemaleFirstNames = (await ReadFile(@"=femalefirstnames.txt")).ToList();
+                    
+                    if (FemaleLastNames.Count == 0)
+                        FemaleLastNames = (await ReadFile(@"=femalelastnames.txt")).ToList();
+
+
+                    First = FemaleFirstNames[Generate.Int(FemaleFirstNames.Count)];
+                    Last = FemaleLastNames[Generate.Int(FemaleLastNames.Count)];
+                    break;
+                }
+            }
+
+            this.PersonGender = PersonGender;
+            DateOfBirth = Generate.NewDateTime(new DateTime(1970, 1, 1), DateTime.UtcNow);
+            RelationshipStatus = (eRelationshipStatus)Generate.Int(4);
+            return this;
+        }
+
+        public static async Task<Person> MakeNew() => await MakeNew((eGender)Generate.Int(2));
+        public static async Task<Person> MakeNew(eGender PersonGender)
+        {
+            var p = new Person();
+            p = await p.FillBlanks(PersonGender);
+            return p;
+        }
+
+        public override string ToString()
+        {
+            return First + " " + Last;
+        }
+    }
+}
